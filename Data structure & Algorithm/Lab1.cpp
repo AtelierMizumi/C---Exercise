8c8e8d249843c3f32faad3ffa057cec6de673fa7
AtelierMizumi <thuanc177@gmail.com>
2024-03-01 11:45:11 +0700

    update lab 1

--- Data structure & Algorithm/Lab1.cpp
+++ Data structure & Algorithm/Lab1.cpp
@@ -12,6 +12,7 @@ int oddCount(int A[], int n);
 double averageEven(int A[], int n);
 void printfromXtoY(int A[], int n, int x, int y);
 int findValueX(int x, int A[], int n);
+void insertValueX(int x, int p, int A[], int *n);
 void overwriteXatPositionP(int x, int p, int A[], int *n);
 void deleteValueAtPositionP(int p, int A[], int *n);
 
@@ -21,14 +22,14 @@ int main(){
     int A[MAX];
     insertArray(A, n);
 
-    cout << "Total: " << sumArray(A, n) << "\n";
+    cout << "Sum of all elements: " << sumArray(A, n) << "\n";
     cout << "Number of odd value(s): " << oddCount(A, n) << "\n";
     cout << "Average between even number(s): " << averageEven(A, n) << "\n";
 
     cout << "Insert x and y to print: ";
     int x, y;
     cin >> x >> y;
-    while (x > y || x < 0) {
+    while (x > y || x < 0 || y > n-1) {
         cout << "Please enter valid x and y again:";
         cin >> x >> y;
     }
@@ -49,6 +50,12 @@ int main(){
     cout << "Enter value and position you want to overwrite: ";
     cin >> x >> p;
     overwriteXatPositionP(x, p, A, ptr);
+    cout << "Array after modified: " << '\n';
+    printArray(A, n);
+
+    cout << "Enter value and position you want to insert: ";
+    cin >> x >> p;
+    insertValueX(x, p, A, ptr);
     cout << "Array after modified: " << '\n';
     printArray(A, n);
 
@@ -111,7 +118,8 @@ void printfromXtoY(int A[], int n, int x, int y){
     }
 }
 int findValueX(int x, int A[], int n){
-    for (int i=0; i<n; i++) {
+    // return -1 if not found
+    for (int i = 0; i < n; i++) {
         if (A[i]==x) {
             return i;
         }
@@ -120,6 +128,21 @@ int findValueX(int x, int A[], int n){
 }
 void overwriteXatPositionP(int x, int p, int A[], int *n){
     A[p]=x;
+}
+void insertValueX(int x, int p, int A[], int *n){
+    int delta=0;
+    int B[MAX];
+    for (int i = 0; i < *n; i++) {
+        B[i] = A[i];
+    }
+    for (int i = 0; i < *n; i++) {
+        if (i==p) {
+            A[i]=x;
+            delta=delta+1;
+        }
+        A[i+delta]=B[i];
+    }
+    *n=*n+1;
 }
 void deleteValueAtPositionP(int p, int A[], int *n){
     int delta=0;
@@ -136,6 +159,7 @@ void deleteValueAtPositionP(int p, int A[], int *n){
 
     // remove final element
     A[*n-1]=0;
+    *n=*n-1;
 }
 
 // clone a array then paste value if = pos skip 
